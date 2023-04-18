eaedf030c0afb52ea03efa9bd7a2c45dc95ade35
Genesis Mendes <genesismendes1@gmail.com>
2023-04-18 15:31:11 -0500

    seed into reviewImages

---
+++ backend/db/seeders/20230418202449-reviewImage-data.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    return queryInterface.bulkInsert(options, [
+
+      { url: "image url", },
+      { url: "image url", },
+      { url: "image url", },
+
+    ])
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'ReviewImages';
+    await queryInterface.bulkDelete(options)
+  }
+};
