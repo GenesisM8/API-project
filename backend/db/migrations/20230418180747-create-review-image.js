3041d4d16977ccf6a4ddda8182105f9786485f0b
Genesis Mendes <genesismendes1@gmail.com>
2023-04-19 12:51:52 -0500

    check spelling

--- backend/db/migrations/20230418180747-create-review-image.js
+++ backend/db/migrations/20230418180747-create-review-image.js
@@ -18,7 +18,7 @@ module.exports = {
         references: {
           model: 'Reviews'
         },
-        onDelete: 'CASCADE'
+        onDelete:'CASCADE'
       },
       url: {
         type: Sequelize.STRING
