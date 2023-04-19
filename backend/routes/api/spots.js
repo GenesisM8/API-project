b21a6aaf057110e0353b0e8021e6f7b9620eefbd
Genesis Mendes <genesismendes1@gmail.com>
2023-04-19 15:33:24 -0500

    get spots of current user working in local dev

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -9,6 +9,32 @@ const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
+//get all spots by current User
+router.get('/current',requireAuth, async (req, res) =>{
+    const allSpots = await Spot.findAll({
+        where: {ownerId: req.user.id}
+    });
+
+    let spots = []
+
+    for (let spot of allSpots) {
+        let jsonSpot = spot.toJSON()
+        const stars = await Review.sum('stars', { where: { spotId: jsonSpot.id } });
+        const total = await Review.count({ where: { spotId: jsonSpot.id } });
+        let avg = stars / total
+        jsonSpot.avgRating = avg
+
+
+        const previewImage = await SpotImage.findOne({ where: { spotId: jsonSpot.id } })
+        if (previewImage) jsonSpot.previewImage = previewImage.url
+
+        spots.push(jsonSpot)
+    }
+
+    return res.json({ spots });
+  
+})
+
 //get all spots
 router.get('/', async (req, res)=>{
     const allSpots = await Spot.findAll()
@@ -32,6 +58,7 @@ for (let spot of allSpots) {
 return res.json({spots});
 
 })
+
 
 
 module.exports = router;
