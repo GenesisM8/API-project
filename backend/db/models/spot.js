9712afcafd4567aa1a5aa3ec7ebf410d14c30e9b
Genesis Mendes <genesismendes1@gmail.com>
2023-04-20 13:30:12 -0500

    create spot works

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -28,6 +28,7 @@ module.exports = (sequelize, DataTypes) => {
         through: models.Review,
         otherKey: 'userId',
         foreignKey: 'spotId',
+
       });
 
 
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,10 +6,59 @@ const { Spot, SpotImage, Review, User } = require('../../db/models');
 
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
+const { Op } = require('sequelize');
 
 const router = express.Router();
 
+const validateSpot = [
+    check('address')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5, max: 150 })
+        .withMessage('Street address is required.'),
 
+    check('city')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5, max: 50 })
+        .withMessage('City is required.'),
+
+    check('state')
+        .exists({ checkFalsy: true })
+        .isAlpha()
+        .isLength({ min: 4, max: 25 })
+        .withMessage('State is required.'),
+
+    check('country')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4, max: 100 })
+        .withMessage('Country is required.'),
+
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -90, max: 90 })
+        .withMessage('Latitude is not valid.'),
+
+    check('lng')
+        .exists({ checkFalsy: true })
+        .isFloat({ min: -180, max: 180 })
+        .withMessage('Longitude is not valid.'),
+
+    check('name')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a name.')
+        .isLength({max: 50})
+        .withMessage('Name must be less than 50 characters.'),
+
+    check('description')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 5, max: 500 })
+        .withMessage('Description is required.'),
+
+    check('price')
+        .exists({ checkFalsy: true })
+        .isNumeric()
+        .withMessage('Price per day is required.'),
+    handleValidationErrors
+];
 
 //get all spots by current User
 router.get('/current', requireAuth, async (req, res) => {
@@ -102,6 +151,25 @@ router.get('/', async (req, res) => {
 
     return res.json({ spots });
 
+})
+
+//create a spot
+router.post('/', validateSpot, requireAuth, async (req, res, next)=>{
+
+    const {address, city, state, country, lat, lng, name, description, price } = req.body
+    const newSpot = await Spot.create({
+        ownerId: req.user.id,
+        address, 
+        city, 
+        state, 
+        country, 
+        lat, 
+        lng, 
+        name,
+        description, 
+        price,
+    })
+ return res.status(201).json(newSpot)
 })
 
 //delete spot
