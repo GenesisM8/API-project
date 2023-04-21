881d3b03f41aed7f9bfbab97e726eaff39d49c8e
Genesis Mendes <genesismendes1@gmail.com>
2023-04-21 17:15:45 -0500

    delete booking works

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -112,7 +112,26 @@ router.put('/:bookingId', requireAuth, async (req, res) => {
 
 })
 
-router.delete('/:bookingId', requireAuth, async (req, res){
+router.delete('/:bookingId', requireAuth, async (req, res)=>{
+    const { user } = req;
+    const id = req.params.bookingId;
+    const booking = await Booking.findByPk(id);
+
+    if (!booking) {
+        return res.status(404).json({ message: "Booking couldn't be found" });
+    }
+
+    if (booking.startDate <= new Date()) {
+        return res.status(403).json({ message: "Bookings that have been started can't be deleted"})
+    }
+    
+    if (booking.userId === user.id) {
+        await booking.destroy();
+        return res.status(200).json({ message: 'Successfully deleted' });
+    } else {
+        return res.status(404).json({message: "You are not authorized to delete this booking."})
+    }
+
     
 })
 
