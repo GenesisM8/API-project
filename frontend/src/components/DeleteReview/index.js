c7fd03551c20b0b32f3979f6dc582fab89ac9b89
Genesis Mendes <genesismendes1@gmail.com>
2023-05-12 01:02:45 -0500

    fixed warning

--- frontend/src/components/DeleteReview/index.js
+++ frontend/src/components/DeleteReview/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch} from 'react-redux';
 import { loadReviewThunk, deleteReviewThunk } from '../../store/reviews'
 import { useModal } from '../../context/Modal';
 import { singleSpotThunk } from '../../store/spots';
